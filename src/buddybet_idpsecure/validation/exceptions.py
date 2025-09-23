386c4486806ad70f76c139bc208fc4349ae133b2
ricardomarzochi07 <ricardomarzochi@gmail.com>
2025-09-23 20:49:09 +0200

    NewVersion_1.0.2

--- src/buddybet_idpsecure/validation/exceptions.py
+++ src/buddybet_idpsecure/validation/exceptions.py
@@ -1,31 +1,32 @@
-class InvalidSignature(Exception):
-    pass
-
-
-class ExpiredToken(Exception):
-    pass
-
-
-class InvalidAudience(Exception):
-    pass
-
-
-class InvalidIssuer(Exception):
-    pass
-
-
-class InvalidNotBefore(Exception):
-    pass
-
-
-class InvalidClaims(Exception):
-    pass
-
-
-class InvalidToken(Exception):
-    pass
-
-
 class JWTValidationError(Exception):
-    pass
     """Base class for JWT validation errors"""
+    pass
+
+
+class InvalidSignature(JWTValidationError):
+    pass
+
+
+class ExpiredToken(JWTValidationError):
+    pass
+
+
+class InvalidAudience(JWTValidationError):
+    pass
+
+
+class InvalidIssuer(JWTValidationError):
+    pass
+
+
+class InvalidNotBefore(JWTValidationError):
+    pass
+
+
+class InvalidClaims(JWTValidationError):
+    pass
+
+
+class InvalidToken(JWTValidationError):
+    pass
+
